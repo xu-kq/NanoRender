59e733a50a6ae329bec1f68729c0fd1cb5df1fed
xukq <xu_kangqi@mail.dlut.edu.cn>
2022-08-28 14:06:27 +0800

    basic develop env.

---
+++ main.cpp
@@ -0,0 +1,19 @@
+// bresenham algorithm
+
+#include<iostream>
+#include<array>
+
+const int Width = 40;
+const int Height = 40;
+//std::array<Vector3f, Width* Height> frame_buffer;
+
+void plot_line(float x1, float x2, float y1, float y2 ) {
+	
+}
+
+void set_pixel(int x, int y, Vector3f&);
+
+int main() {
+
+	return 0;
+}
